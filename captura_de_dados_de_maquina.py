0277623c40b5081b4c1a865d4f0abbd907f1fa44
Bruna Santana Reginato <160195693+BrunaReginat0@users.noreply.github.com>
2024-08-22 10:41:38 -0300

    adicionei a captura de dados de máquina ✨

---
+++ captura_de_dados_de_maquina.py
@@ -0,0 +1,39 @@
+import psutil
+import time
+import mysql.connector
+
+i = 0
+
+db_connection = mysql.connector.connect(host='localhost', user='root', password='150621', database='testeAutomacao')
+cursor = db_connection.cursor()
+
+
+while True:
+    UsoDeCPU = psutil.cpu_percent(interval=1)
+    FreqDeCPU = psutil.cpu_freq()
+    UsoDeMemo = psutil.virtual_memory()
+    UsoDeDisco = psutil.disk_usage('C:\\')
+    i = i + 1
+
+    sql = "INSERT INTO registros (percentualCPU, usoCPU, usoMemoria, usoDisco) VALUES (%s, %s, %s, %s)"
+    values = (FreqDeCPU.current, UsoDeCPU, UsoDeMemo.percent, UsoDeDisco.percent)
+    cursor.execute(sql, values)
+    db_connection.commit()
+
+    print(
+    """
+    {:.1f}º CAPTURA
+    ----------------------------------
+    Percentual de uso de CPU: {:.2f}%
+    Frequência da CPU: {:.2f} MHz
+
+    Percentual de uso da Memória: {:.2f}%
+
+    Percentual de uso do Disco: {:.2f}%
+
+    """.format(i, UsoDeCPU, FreqDeCPU.current, UsoDeMemo.percent, UsoDeDisco.percent))
+    time.sleep(3)
+
+
+
+    
