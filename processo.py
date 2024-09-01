12b4994183083b587626958c2d5f0cf287169f54
BrunaReginat0 <bruna.reginato@sptech.school>
2024-08-31 23:08:20 -0300

    removi arquivo teste

--- processo.py
+++
@@ -1,9 +0,0 @@
-import psutil
-
-for process in psutil.process_iter():
-        print(f"ID: {process.pid}, Nome: {process.name()}, Status: {process.status()}")
-
-        print(psutil.sensors_battery()[0])
-        print(psutil.sensors_battery()[1])
-        print(psutil.sensors_battery()[2])
-        print(psutil.users()[0].name)
