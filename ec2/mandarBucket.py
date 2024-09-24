d046075afbfc0f1c165a4f52761f30e9f0fd0a20
Ivanrangelpm <ivan.marcolin@sptech.school>
2024-09-24 20:56:47 -0300

    finalizando codigo para mandar ao bucket

--- ec2/mandarBucket.py
+++ ec2/mandarBucket.py
@@ -1,7 +1,20 @@
 import boto3
-from datetime import datetime
-s3_resource = boto3.resource("s3")
-bucket = s3_resource.Bucket("tagtech-raw")
+import os
 
+s3 = boto3.client('s3')
 
+bucket_name = 'seu-bucket-s3'
+pasta_local = 'ec2/csv'
 
+for arquivo in os.listdir(pasta_local):
+    caminho_arquivo = os.path.join(pasta_local, arquivo)
+    
+    try:
+        s3.upload_file(caminho_arquivo, bucket_name, arquivo)
+        print(f"Arquivo {arquivo} enviado para o bucket {bucket_name} com sucesso!")
+        
+        os.remove(caminho_arquivo)
+        print(f"Arquivo {arquivo} apagado da pasta local.")
+    
+    except Exception as e:
+        print(f"Erro ao enviar ou apagar o arquivo {arquivo}: {e}")
