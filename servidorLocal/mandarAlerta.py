5c6d6dced1393739baaf7c623b8277dff6ffd034
Ivanrangelpm <ivan.marcolin@sptech.school>
2024-09-28 20:51:04 -0300

    adicionando dotenv no projeto para segurança dos tokens

--- servidorLocal/mandarAlerta.py
+++ servidorLocal/mandarAlerta.py
@@ -3,6 +3,12 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+#pip install python-dotenv
+import dotenv
+import os
+
+dotenv.load_dotenv(dotenv.find_dotenv())
+
 
 url = "https://tagtech.atlassian.net//rest/api/2/issue"
 
